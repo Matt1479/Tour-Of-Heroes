d4c34ec599a95bb5cfa80cc09bd2eb4e0f6f47a1
Matt1479 <gitmatt1479@gmail.com>
2022-04-20 11:32:00 +0200

    refactor(global): use async not subscribtion

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,37 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-
-  heroes: Hero[] = []; // declaration of an array
+  heroes$: Observable<Hero[]>;
 
   // heroes = HEROES; we don't need this anymore
-  
+
   // hero: Hero = {
   //   id: 1,
   //   name: 'Windstorm'
   // };
 
-  getHeroes(): void {
-    this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
-  }
+  // getHeroes(): void {}
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor(private heroService: HeroService) { }
-
-  ngOnInit(): void {
-    this.getHeroes();
+  constructor(private heroService: HeroService) {
+    this.heroes$ = this.heroService.getHeroes();
   }
 
+  ngOnInit(): void {}
 }
